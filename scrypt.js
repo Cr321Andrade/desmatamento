d1d10b05d69578059bc9a3270398ef821a13af06
Cr321Andrade <147090353+Cr321Andrade@users.noreply.github.com>
2025-05-27 17:05:52 -0300

    Create scrypt.js

---
+++ scrypt.js
@@ -0,0 +1,7 @@
+document.getElementById('campoBtn').addEventListener('click', function() {
+    alert('No campo, a tecnologia tem permitido a automação das colheitas, uso de drones para monitoramento e muito mais!');
+});
+
+document.getElementById('cidadeBtn').addEventListener('click', function() {
+    alert('Nas cidades, a tecnologia facilita o transporte com carros autônomos, gerenciamento inteligente de tráfego e serviços urbanos conectados.');
+});
